4816ab2803d0ab0f1b64fa708bc820f963740d59
Simp <fedormarkuzov598@gmail.com>
2025-12-25 22:45:15 +0300

    Update FileManager.h

--- src/FileManager.h
+++ src/FileManager.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <vector>
+#include <string>
+
+struct Account {
+    int id;
+    std::string name;
+    double balance;
+};
+
+void saveAccounts(const std::vector<Account>& accounts);
+std::vector<Account> loadAccounts();
