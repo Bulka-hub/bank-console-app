2f833c29a014cda6c92a012c7511d81c64f77530
Bulka Daniil <qerqee793@gmail.com>
2025-12-26 23:41:37 +0300

    sdf

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,7 @@ using namespace std;
 
 int main() {
 	cout << "Bank console app - çàïóùåí";
-
-
-
-	/*
-		Bank myBank;
-		myBank.createAccount(1, "Ivan", 1000);
-		myBank.createAccount(2, "Petr", 500);
-
-		// Попытка перевода
-		bool success = myBank.transferMoney(1, 2, 200);
-		// Теперь у Ивана 800, у Петра 700
-	*/
-
-
-
+	std::cout << "\nНажмите Enter для выхода...";
+	std::cin.get();
 	return 0;
 }
